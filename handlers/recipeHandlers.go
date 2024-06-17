7c3d10f8ef7868b6d05d1655ec2588c658fafb7a
Kaleab Alemayehu (Neo) <cipherlofi04@gmail.com>
2024-06-17 11:32:06 -0300

    add file upload action and file handling hook

--- handlers/recipeHandlers.go
+++ handlers/recipeHandlers.go
@@ -1,32 +1,148 @@
 package handlers
 
 import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
 	"net/http"
-	"path/filepath"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
-func Upload(c *gin.Context){
-	recipeID := c.PostForm("recipe_id")
-    form, err := c.MultipartForm()
-    if err != nil {
-        c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid form data"})
-        return
-    }
-
-    files := form.File["images"]
-    var imageUrls []string
-
-    for _, file := range files {
-        // Save the file locally 
-        filePath := filepath.Join("uploads", file.Filename)
-        if err := c.SaveUploadedFile(file, filePath); err != nil {
-            c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save file"})
-            return
-        }
-        imageUrls = append(imageUrls, filePath)
-    }
-
-    c.JSON(http.StatusOK, gin.H{"recipe_id": recipeID, "image_urls": imageUrls})
+type imageUploadArgs struct {
+	Name string `json:"name"`
+	Base64Str string `json:"base64Str"`
 }
+type ActionPayload struct {
+	SessionVariables map[string]interface{} `json:"session_variables"`
+	Input imageUploadArgs `json:"input"`
+}
+
+type GraphQLError struct {
+	Message string `json:"message"`
+}
+type saveImageOutput struct{
+	ImageUrl string `json:"image_url"`
+}
+
+
+func saveImageToFile(input imageUploadArgs) (saveImageOutput , error) {
+	// fmt.Printf("name: %v", string(input.Name))
+	// fmt.Printf("data: %v", string(input.Base64Str))
+	// saveDir, err := filepath.Abs("../uploads")
+	// if err != nil {
+	// 	panic("unable to find save directory")
+	// }
+	// create a decoder with the base64 string from request
+	dec, err := base64.StdEncoding.DecodeString(string(input.Base64Str))
+	if err != nil {
+		panic(err);
+	}
+
+	// create file and wait to close it after the function is about to return
+	file, err := os.Create(fmt.Sprintf("%v",  fmt.Sprint(input.Name)))
+	if err != nil {
+		panic("unable to create a file in the upload directory!")
+	}
+	defer file.Close()
+	// write the byte to the file
+	if _ , err = file.Write(dec); err != nil{
+		panic("unable to write file")
+	}
+	//  save the file 
+	if err := file.Sync(); err != nil {
+        panic(err)
+    }
+	
+	var image saveImageOutput
+	return image , err
+}
+
+func Upload (c *gin.Context){
+	var jsonData map[string]interface{}
+	// convert the byte( i think it is what it is) from gin.context to json
+	if err := c.ShouldBindJSON(&jsonData); err != nil{
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+			"errorCode": 1,
+		})
+	}
+	// fmt.Println(string(jsonData))
+
+	//  marshal it to jsonString (map[string]Interface to json string but bytes)
+	jsonString, err:= json.Marshal(jsonData)
+	if err != nil{
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+			"errorCode": 2,
+		})
+	}
+	// unmarshal itto actionPayload
+	var actionPayload ActionPayload
+	err = json.Unmarshal(jsonString, &actionPayload)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+			"errorCode": 3,
+		})
+	}
+	// send it to save the file
+	output, err := saveImageToFile(actionPayload.Input)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "unable to save file",
+			"error": err.Error(),
+			"errorCode": 3,
+		})
+	}
+
+	c.JSON(http.StatusCreated,output)
+
+	// func imageUpload(args imageUploadArgs) (response imageOutput, err error) {
+		// response =  imageOutput {
+		//   Image_url: "<sample value>",
+		// }
+		// return response, nil
+}
+// func Uploads(c *gin.Context){
+// 	recipeID := c.PostForm("recipe_id")
+//     form, err := c.MultipartForm()
+//     if err != nil {
+//         c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid form data"})
+//         return
+//     }
+//     files := form.File["images"]
+//     var imageUrls []string
+
+//     for _, file := range files {
+//         // Save the file locally 
+//         filePath := filepath.Join("uploads", file.Filename)
+//         if err := c.SaveUploadedFile(file, filePath); err != nil {
+//             c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save file"})
+//             return
+//         }
+//         imageUrls = append(imageUrls, filePath)
+// 	fmt.Print(filePath)
+//     } 
+	
+//     c.JSON(http.StatusOK, gin.H{"recipe_id": recipeID, "image_urls": imageUrls[0]})
+// }
+// func Upload( c *gin.Context){
+// 	image, err := c.FormFile("image")
+// 	if err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{ "error": "Invalid form data", "message": err.Error()})
+// 		return
+// 	}
+// 	dst := "./uploads/" + image.Filename
+// 	if err = c.SaveUploadedFile(image, dst); err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{ "error" : "unable to upload image", "message": err.Error()})
+// 		return
+// 	}
+// 	c.JSON(http.StatusOK, gin.H{ "message": "uploaded successfully", "data": image.Filename})
+// }
+
+						
+
+
--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"os"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/kaleabAlemayehu/foodopia/handlers"
@@ -14,16 +16,25 @@ func main() {
 
 	err := godotenv.Load(".env")
 
+
 	utility.CheckError(err, "error to load .env file")
 
 	port := os.Getenv("GIN_PORT")
 
 	r := gin.Default()
+	r.Use(cors.New(
+		cors.Config{
+			AllowOrigins:     []string{"http://localhost:3000"},
+			AllowMethods:     []string{"POST", "GET", "OPTIONS"},
+			AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+			ExposeHeaders:    []string{"Content-Length"},
+			AllowCredentials: true,
+			MaxAge: 12 * time.Hour,
+	}))
 	r.POST("/signup", handlers.Signup)
 	r.POST("/login", handlers.Login)
 	r.POST("/graphql", middlewares.CheckAuth, handlers.ProxyToHasura)
 	r.POST("/upload", handlers.Upload);
 	r.POST("/welcome", handlers.SendEmail);
 	r.Run(":" + port)
-
 }
