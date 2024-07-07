b3808efe264290c472c5977d6c17acee780afe1a
neoCipher (Kaleab Alemayehu) <cipherlofi04@gmail.com>
2024-07-06 20:31:55 -0400

    refactor: remove checkError utility

--- main.go
+++ main.go
@@ -9,15 +9,15 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/kaleabAlemayehu/foodopia/handlers"
 	"github.com/kaleabAlemayehu/foodopia/middlewares"
-	"github.com/kaleabAlemayehu/foodopia/utility"
 )
 
 func main() {
 
 	err := godotenv.Load(".env")
 
-
-	utility.CheckError(err, "error to load .env file")
+	if err != nil {
+		panic("error to load .env file")
+	}
 
 	port := os.Getenv("GIN_PORT")
 
@@ -29,12 +29,12 @@ func main() {
 			AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 			ExposeHeaders:    []string{"Content-Length"},
 			AllowCredentials: true,
-			MaxAge: 12 * time.Hour,
-	}))
+			MaxAge:           12 * time.Hour,
+		}))
 	r.POST("/signup", handlers.Signup)
 	r.POST("/login", handlers.Login)
 	r.POST("/graphql", middlewares.CheckAuth, handlers.ProxyToHasura)
-	r.POST("/upload", handlers.Upload);
-	r.POST("/welcome", handlers.SendEmail);
+	r.POST("/upload", handlers.Upload)
+	r.POST("/welcome", handlers.SendEmail)
 	r.Run(":" + port)
 }
