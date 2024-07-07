14e2c7b09841db09aad2167960f78003160d48d6
neoCipher (Kaleab Alemayehu) <cipherlofi04@gmail.com>
2024-07-06 20:28:55 -0400

    add user login hook handler

--- handlers/userHandlers.go
+++ handlers/userHandlers.go
@@ -15,7 +15,6 @@ import (
 
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/hasura/go-graphql-client"
-	"github.com/kaleabAlemayehu/foodopia/utility"
 	"github.com/tidwall/gjson"
 
 	"github.com/gin-gonic/gin"
@@ -26,6 +25,11 @@ type Body struct {
 	Username string `json:"name"  binding:"required"`
 	Email    string `json:"email"  binding:"required"`
 	Password string `json:"password"  binding:"required"`
+}
+type LoginResponse struct {
+	Id       int64  `json:"id"`
+	Email    string `json:"email"`
+	Username string `json:"username"`
 }
 type Params struct {
 	Body Body `json:"params"`
@@ -101,7 +105,7 @@ func Signup(c *gin.Context) {
 		panic(err)
 	}
 
-	//  marshal it to jsonString (map[string]Interface to json string but bytes)
+	//marshal it to jsonString (map[string]Interface to json string but bytes)
 	jsonString, err := json.Marshal(jsonData)
 	if err != nil {
 		panic(err)
@@ -122,24 +126,7 @@ func Signup(c *gin.Context) {
 		})
 	}
 
-	/*
-		jwt.MapClaims{
-			"id":          user.ID,
-			"firstName":   user.FirstName,
-			"lastName":    user.LastName,
-			"email":       user.Email,
-			"phoneNumber": user.PhoneNumber,
-			"exp":         expiryDate,
-			"https://hasura.io/jwt/claims": map[string]interface{}{
-				"x-hasura-default-role":  "user",
-				"x-hasura-allowed-roles": [2]string{"user", "admin"},
-				"x-hasura-user-id":       strconv.Itoa(user.ID),
-			},
-
-	*/
-
-	// Generate JWT token
-
+	// add claim
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"id":       userId,
 		"email":    actionPayload.Input.Body.Email,
@@ -151,6 +138,7 @@ func Signup(c *gin.Context) {
 			"x-hasura-user-id":       strconv.Itoa(int(userId)),
 		},
 	})
+	// Generate JWT token
 	tokenString, err := token.SignedString([]byte(os.Getenv("MY_SECRET")))
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
@@ -166,84 +154,94 @@ func Signup(c *gin.Context) {
 		"token": tokenString,
 	})
 }
-
-func Login(c *gin.Context) {
-	// get query form query.go
-	query := utility.CheckUser
-	// loading GQL URL
-	var GQLURL string = os.Getenv("GRAPHQL_URI")
-	if GQLURL == "" {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "GRAPHQL_URI not set",
-		})
-		return
-	}
-	// get email and pass of from req body
-	var body Body
-	if err := c.Bind(&body); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "failed to read body",
-		})
-		return
-	}
-	// preparing payload
-	checkPayload := map[string]interface{}{
-		"query": query,
-		"variables": map[string]interface{}{
-			"email": body.Email,
+func CheckUser(input Body) (LoginResponse, error) {
+	// create client
+	adminSecret := os.Getenv("ADMIN_SECRET")
+	gqlUrl := os.Getenv("GRAPHQL_URL")
+	client := graphql.NewClient(gqlUrl, &http.Client{
+		Transport: headerRoundTripper{
+			setHeaders: func(req *http.Request) {
+				req.Header.Set(xHasuraAdminSecret, adminSecret)
+			},
+			rt: http.DefaultTransport,
 		},
-	}
-
-	checkPayloadBytes, err := json.Marshal(checkPayload)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "failed to marshal check payload",
-		})
-		return
-	}
-
-	//  lookup registered user
-	checkRes, err := http.Post(GQLURL, "application/json", bytes.NewBuffer(checkPayloadBytes))
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "failed to check user",
-		})
-		return
-	}
-	defer checkRes.Body.Close()
-	var checkResult map[string]interface{}
-	if err := json.NewDecoder(checkRes.Body).Decode(&checkResult); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "failed to read check response",
-		})
-		return
-	}
-
-	users, ok := checkResult["data"].(map[string]interface{})["users"].([]interface{})
-	if !ok || len(users) == 0 {
-		c.JSON(http.StatusUnauthorized, gin.H{
-			"error": "user not found",
-		})
-		return
-	}
-
-	user := users[0].(map[string]interface{})
-	storedPasswordHash := user["password_hash"].(string)
-
-	// Compare provided password with stored password hash
-	if err := bcrypt.CompareHashAndPassword([]byte(storedPasswordHash), []byte(body.Password)); err != nil {
-		c.JSON(http.StatusUnauthorized, gin.H{
-			"error": "invalid password",
-		})
-		return
-	}
-	log.Printf("user log: %v", user)
-	// Generate JWT token
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"user_id": user["id"],
-		"exp":     time.Now().Add(time.Hour * 24 * 30).Unix(),
 	})
+	// create query
+	var q struct {
+		Users []struct {
+			Username     string `graphql:"username"`
+			PasswordHash string `graphql:"password_hash"`
+			Email        string `graphql:"email"`
+			Id           int64  `graphql:"id"`
+		} `graphql:"users(where: {email: {_eq: $email}})"`
+	}
 
+	// create varaiable
+	variable := map[string]interface{}{
+		"email": string(input.Email),
+	}
+
+	// send request to hasura
+	err := client.Query(context.Background(), &q, variable, graphql.OperationName("CheckUser"))
+	if err != nil {
+		log.Fatalf("error Occured: %v\n", err)
+	}
+	// check if the password is correct
+	storedPassword := q.Users[0].PasswordHash
+	if storedPassword == "" {
+		panic("there is no password")
+	}
+	if err := bcrypt.CompareHashAndPassword([]byte(storedPassword), []byte(input.Password)); err != nil {
+		panic("password is not the same!")
+	}
+
+	return LoginResponse{
+		Id:       q.Users[0].Id,
+		Email:    q.Users[0].Email,
+		Username: q.Users[0].Username,
+	}, nil
+
+}
+func Login(c *gin.Context) {
+	// read the data from the request
+	var jsonData map[string]interface{}
+	// convert the byte( i think it is what it is) from gin.context to json
+	if err := c.ShouldBindJSON(&jsonData); err != nil {
+		panic(err)
+	}
+
+	//marshal it to jsonString (map[string]Interface to json string but bytes)
+	jsonString, err := json.Marshal(jsonData)
+	if err != nil {
+		panic(err)
+	}
+
+	// unmashall it to UserActionPayload
+	var actionPayload UserActionPayload
+	err = json.Unmarshal(jsonString, &actionPayload)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid payload"})
+		return
+	}
+	// check if the user is legit
+	res, err := CheckUser(actionPayload.Input.Body)
+	if err != nil {
+		panic("there is fucking error")
+	}
+
+	// create a claim
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id":       res.Id,
+		"email":    res.Email,
+		"username": res.Username,
+		"exp":      time.Now().Add(time.Hour * 24 * 30).Unix(),
+		"https://hasura.io/jwt/claims": map[string]interface{}{
+			"x-hasura-default-role":  "user",
+			"x-hasura-allowed-roles": [2]string{"user", "admin"},
+			"x-hasura-user-id":       strconv.Itoa(int(res.Id)),
+		},
+	})
+	// generate jwt token
 	tokenString, err := token.SignedString([]byte(os.Getenv("MY_SECRET")))
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
@@ -251,12 +249,12 @@ func Login(c *gin.Context) {
 		})
 		return
 	}
-	//
-	c.SetSameSite(http.SameSiteLaxMode)
-	c.SetCookie("Authorization", tokenString, 3600*24*30, "", "", false, true)
-	// Send it back
+
 	c.JSON(http.StatusOK, gin.H{
-		"message": "succefully logged in",
+		"id":    res.Id,
+		"name":  res.Username,
+		"email": res.Email,
+		"token": tokenString,
 	})
 
 }
