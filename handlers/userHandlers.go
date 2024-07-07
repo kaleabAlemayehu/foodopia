c965b79c9bf93b61871341d61d74dfe864b36537
neoCipher (Kaleab Alemayehu) <cipherlofi04@gmail.com>
2024-07-06 21:18:42 -0400

    refactoring: useing CreateToken for loginHandler

--- handlers/userHandlers.go
+++ handlers/userHandlers.go
@@ -8,10 +8,7 @@ import (
 	"net/http"
 	"net/smtp"
 	"os"
-	"strconv"
-	"time"
 
-	"github.com/golang-jwt/jwt/v5"
 	"github.com/hasura/go-graphql-client"
 	"github.com/kaleabAlemayehu/foodopia/models"
 	"github.com/kaleabAlemayehu/foodopia/utility"
@@ -191,26 +188,8 @@ func Login(c *gin.Context) {
 		panic("there is fucking error")
 	}
 
-	// create a claim
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"id":       res.Id,
-		"email":    res.Email,
-		"username": res.Username,
-		"exp":      time.Now().Add(time.Hour * 24 * 30).Unix(),
-		"https://hasura.io/jwt/claims": map[string]interface{}{
-			"x-hasura-default-role":  "user",
-			"x-hasura-allowed-roles": [2]string{"user", "admin"},
-			"x-hasura-user-id":       strconv.Itoa(int(res.Id)),
-		},
-	})
 	// generate jwt token
-	tokenString, err := token.SignedString([]byte(os.Getenv("MY_SECRET")))
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "failed to generate token",
-		})
-		return
-	}
+	tokenString := utility.CreateToken(res)
 
 	c.JSON(http.StatusOK, gin.H{
 		"id":       res.Id,
