7d707e7eaa043df022811a6df98fdaa41e86d560
neoCipher (Kaleab Alemayehu) <cipherlofi04@gmail.com>
2024-07-06 21:46:16 -0400

    refactoring: refactoring upload image

--- handlers/recipeHandlers.go
+++ handlers/recipeHandlers.go
@@ -1,67 +1,14 @@
 package handlers
 
 import (
-	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"os"
-	"path/filepath"
 
 	"github.com/gin-gonic/gin"
+	"github.com/kaleabAlemayehu/foodopia/models"
+	"github.com/kaleabAlemayehu/foodopia/utility"
 )
-
-type imageUploadArgs struct {
-	Name      string `json:"name"`
-	Base64Str string `json:"base64Str"`
-}
-type ActionPayload struct {
-	SessionVariables map[string]interface{} `json:"session_variables"`
-	Input            imageUploadArgs        `json:"input"`
-}
-
-type GraphQLError struct {
-	Message string `json:"message"`
-}
-type saveImageOutput struct {
-	ImageUrl string `json:"image_url"`
-}
-
-func saveImageToFile(input imageUploadArgs) (saveImageOutput, error) {
-	var image saveImageOutput
-
-	// create a decoder with the base64 string from request
-	dec, err := base64.StdEncoding.DecodeString(string(input.Base64Str))
-	if err != nil {
-		panic(err)
-	}
-
-	dir, err := filepath.Abs("./uploads")
-	if err != nil {
-		panic(err)
-	}
-	// create file and wait to close it after the function is about to return
-	file, err := os.Create(filepath.Join(dir, input.Name))
-	if err != nil {
-		// panic("unable to create a file in the upload directory!")
-		panic(err)
-	}
-	defer file.Close()
-	// write the byte to the file
-	if _, err = file.Write(dec); err != nil {
-		panic(err)
-	}
-	//  save the file
-	if err := file.Sync(); err != nil {
-		panic(err)
-	}
-
-	image.ImageUrl, err = filepath.Abs(filepath.Join(dir, input.Name))
-	if err != nil {
-		panic(err)
-	}
-	return image, err
-}
 
 func Upload(c *gin.Context) {
 	var jsonData map[string]interface{}
@@ -77,14 +24,14 @@ func Upload(c *gin.Context) {
 		panic(err)
 	}
 	// unmarshal itto actionPayload
-	var actionPayload ActionPayload
+	var actionPayload models.ActionPayload
 	err = json.Unmarshal(jsonString, &actionPayload)
 
 	if err != nil {
 		panic(err)
 	}
 	// send it to save the file
-	output, err := saveImageToFile(actionPayload.Input)
+	output, err := utility.SaveImageToFile(actionPayload.Input)
 	if err != nil {
 		panic(err)
 	}
@@ -94,40 +41,3 @@ func Upload(c *gin.Context) {
 	})
 
 }
-
-// func Uploads(c *gin.Context){
-// 	recipeID := c.PostForm("recipe_id")
-//     form, err := c.MultipartForm()
-//     if err != nil {
-//         c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid form data"})
-//         return
-//     }
-//     files := form.File["images"]
-//     var imageUrls []string
-
-//     for _, file := range files {
-//         // Save the file locally
-//         filePath := filepath.Join("uploads", file.Filename)
-//         if err := c.SaveUploadedFile(file, filePath); err != nil {
-//             c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save file"})
-//             return
-//         }
-//         imageUrls = append(imageUrls, filePath)
-// 	fmt.Print(filePath)
-//     }
-
-//     c.JSON(http.StatusOK, gin.H{"recipe_id": recipeID, "image_urls": imageUrls[0]})
-// }
-// func Upload( c *gin.Context){
-// 	image, err := c.FormFile("image")
-// 	if err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{ "error": "Invalid form data", "message": err.Error()})
-// 		return
-// 	}
-// 	dst := "./uploads/" + image.Filename
-// 	if err = c.SaveUploadedFile(image, dst); err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{ "error" : "unable to upload image", "message": err.Error()})
-// 		return
-// 	}
-// 	c.JSON(http.StatusOK, gin.H{ "message": "uploaded successfully", "data": image.Filename})
-// }
--- models/model.go
+++ models/model.go
@@ -14,3 +14,16 @@ type UserActionPayload struct {
 	SessionVariables map[string]interface{} `json:"session_variables"`
 	Input            Params                 `json:"input"`
 }
+
+type ImageUploadArgs struct {
+	Name      string `json:"name"`
+	Base64Str string `json:"base64Str"`
+}
+type ActionPayload struct {
+	SessionVariables map[string]interface{} `json:"session_variables"`
+	Input            ImageUploadArgs        `json:"input"`
+}
+
+type SaveImageOutput struct {
+	ImageUrl string `json:"image_url"`
+}
